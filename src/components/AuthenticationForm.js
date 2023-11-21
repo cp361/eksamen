bd4ad3a49017adc8df475901f955fb49b4cb4e71
rattodk <rattodesigner@gmail.com>
2023-11-21 22:47:24 +0100

    AUthenticaitonLoader
    Forsøg på at lave loader(s)

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -10,17 +10,17 @@ import {
   Group,
   Button,
   Center,
-} from "@mantine/core"
+  Loader,
+} from "@mantine/core";
 
-import classes from "../styles/AuthenticationTitle.module.css"
-import { supabase } from "../pages/lib/helper/supabaseClient.js"
-import React, { useEffect, useState } from "react"
-import { createClient } from "@supabase/supabase-js"
-import { useFavicon } from "@mantine/hooks"
-import { useRouter } from "next/router"
+import classes from "../styles/AuthenticationTitle.module.css";
+import { supabase } from "../pages/lib/helper/supabaseClient.js";
+import React, { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useFavicon } from "@mantine/hooks";
+import { useRouter } from "next/router";
 
 export function AuthenticationForm() {
-  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
@@ -28,51 +28,58 @@ export function AuthenticationForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       const { user, session, error } = await supabase.auth.signInWithPassword({
         email,
         password,
       });
       if (error) throw error;
-      router.replace('/home')
+      router.replace("/home");
     } catch (error) {
       alert(error.message);
+    } finally {
+      setLoading(false);
     }
   };
-  
+}
 
-  return (
-    <Container h="{430}" pt={30} className="form-bg">
-      <Center>
-        <div className="logo">
-          <span className="logo-left"></span>
-          <span className="logo-right"></span>
-          <span className="logo-top"></span>
-          <span className="logo-bottom"></span>
-        </div>
-      </Center>
+return (
+  <Container h="{430}" pt={30} className="form-bg">
+    <Center>
+      <div className="logo">
+        <span className="logo-left"></span>
+        <span className="logo-right"></span>
+        <span className="logo-top"></span>
+        <span className="logo-bottom"></span>
+      </div>
+    </Center>
 
-      {/* CPH Business teksten */}
-      <p className="logo-text">
-        <span className="logo-first-text">cph</span>business
-      </p>
+    {/* CPH Business teksten */}
+    <p className="logo-text">
+      <span className="logo-first-text">cph</span>business
+    </p>
+
+    <div>
+      <div>
+        <Title ta="center" className={classes.title}>
+          Velkommen tilbage!
+        </Title>
+        <Text
+          c="dimmed"
+          size="sm"
+          ta="center"
+          mt={5}
+          onClick={() => router.push("/signup")}
+        >
+          Har du ikke en bruger endnu?
+          <Anchor size="sm" component="button" ml={5}>
+            Opret bruger
+          </Anchor>
+        </Text>
+      </div>
 
       <div>
-        <div>
-          <Title ta="center" className={classes.title}>
-            Velkommen tilbage!
-          </Title>
-          <Text c="dimmed" size="sm" ta="center" mt={5} onClick={() => router.push('/signup')}>
-            Har du ikke en bruger endnu?
-            <Anchor size="sm" component="button" ml={5}>
-              Opret bruger
-            </Anchor>
-          </Text>
-        </div>
-
-        <div>
-        <form
-          onSubmit={handleSubmit}
-        >
+        <form onSubmit={handleSubmit}>
           <input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -87,9 +94,7 @@ export function AuthenticationForm() {
           />
           <button type="submit">Login</button>
         </form>
-        </div>
-        
       </div>
-    </Container>
-  )
-}
+    </div>
+  </Container>
+);
