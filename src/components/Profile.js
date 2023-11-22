21d625ee6fc42917b78885e37e9118a5d6c7751a
Casper Pedersen <116882875+cp361@users.noreply.github.com>
2023-11-22 14:31:48 +0100

    lidt styling til profil boksen + logud knap (virker ikke endnu)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Avatar } from '@mantine/core';
+import { Container, Avatar, Button } from '@mantine/core';
 
 
 
@@ -8,16 +8,21 @@ import { Container, Avatar } from '@mantine/core';
 export function Profile() {
 
   return (
-    <Container className='profile-container' fluid h={100} bg="#FBB040">
+    <Container className='profile-container' fluid h={100} bg="var(--cphYellow)">
       <div className='profile-info'>
         <div className='profile-photo'>
           <Avatar className='profile-avatar' variant='filled' color="blue" radius="xl">MJ</Avatar>
         </div>
         <div className='profile-text'>
+          
+          {/* Vi skal også have lavet dette om til, så den viser navnet på brugeren der er logget ind */}
           <h1>Hi Mia Jensen</h1>
           <p>Welcome back!</p>
         </div>
+
+        
       </div>
+        <Button>Logout</Button>
     </Container>
   );
 };
