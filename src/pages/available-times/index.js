46f1e0f10c497b2e82da517808b920afbe59c21d
Casper Pedersen <116882875+cp361@users.noreply.github.com>
2023-11-23 14:04:53 +0100

    jaja

--- src/pages/available-times/index.js
+++ src/pages/available-times/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid, GridCol } from '@mantine/core'
 import Image from 'next/image'
 import sideImg from '../../assets/sidebar-background.png'
@@ -9,6 +9,27 @@ import { FaReceipt } from "react-icons/fa";
 import { FaRegCalendarAlt } from "react-icons/fa";
 
 const availableTimes = () => {
+
+  const getClassRooms = async () => {
+    const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRocG1keGhudnltZ2h3eGpucWtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1MTA2MzYsImV4cCI6MjAxNjA4NjYzNn0.iSvURo8-6F-ns9QmFtEnuBE0pvQdHw6vf2VaMUAVQz0'
+    const data = await fetch('https://thpmdxhnvymghwxjnqko.supabase.co/rest/v1/classrooms', {
+      headers: {
+        apikey: anonKey,
+        Authorization: 'Bearer' `${anonKey}`,
+      }
+    }).then(response => response.json())
+
+    setClassRooms(data);
+  }
+
+  useEffect(() => {
+    getClassRooms();
+  }, []);
+
+  const createReservation = async (reservation) => {
+    
+  }
+  
   return (
     
     <div className="booking div-text">
