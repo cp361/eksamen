92a9f9d36e943b7f963381c35d0b7b428923541f
Casper Pedersen <cph-cp361@cphbusiness.dk>
2023-12-04 16:48:09 +0100

    jaja

--- src/pages/available-times/index.js
+++ src/pages/available-times/index.js
@@ -32,14 +32,9 @@ const availableTimes = () => {
   useEffect(() => {
     const fetchClassRooms = async () => {
       const { data, error } = await supabase
-<<<<<<< Updated upstream
         .from("classrooms")
         .select("")
         .limit(2);
-=======
-        .from('classrooms')
-        .select('')
->>>>>>> Stashed changes
 
       if (error) {
         setFetchError("Could not fetch class rooms");
