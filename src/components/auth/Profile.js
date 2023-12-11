3f55fc2f6018786df99d30ac1f54002f00878085
Casper Pedersen <116882875+cp361@users.noreply.github.com>
2023-12-11 23:11:29 +0100

    prøvede at få brugerens email/id vist

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -1,37 +1,69 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import { Container, Avatar, Button } from '@mantine/core';
 import { useRouter } from 'next/router';
+import { supabase } from '@/pages/lib/helper/supabaseClient';
 
 
 
 
 
 export function Profile() {
+  const [userEmail, setUserEmail] = useState(null);
+  const router = useRouter();
 
+  useEffect(() => {
+    retrieveUser();
+  }, []);
 
   const retrieveUser = async () => {
-    const { data: { user } } = await supabase.auth.getUser()
+    try {
+      const { data, error } = await supabase.auth.user();
 
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        // Opdater state med brugerens e-mail
+        setUserEmail(data.email);
+        console.log(data)
+      }
+    } catch (error) {
+      console.error('Error retrieving user:', error.message);
+    }
+  };
+
+  // const session = supabase.auth.getSession();
+  // if (session) {
+  //   // Brugeren er logget ind
+  //   // Gennemfør yderligere handlinger her
+  //   console.log(session?.user);
+  // }
+
+  const test = async () => {
+
+    const { data, error } = await supabase.auth.refreshSession()
+    const { session, user } = data
+    console.log(session?.user);
   }
-  
-
-  const router = useRouter();
 
   return (
     <Container className='profile-container' fluid h={100} bg="var(--cphYellow)">
       <div className='profile-info'>
         <div className='profile-photo'>
           <Avatar
-          className='profile-avatar'
-          variant='filled'
-          color="var(--cphBlue)"
-          radius="xl">
+            className='profile-avatar'
+            variant='filled'
+            color="var(--cphBlue)"
+            radius="xl">
           </Avatar>
         </div>
         <div className='profile-text'>
 
           {/* Vi skal også have lavet dette om til, så den viser navnet på brugeren der er logget ind */}
-          <h1>Hi User</h1>
+          <h1>Hi {userEmail || 'User'} </h1>
           <p>Welcome back!</p>
         </div>
 
