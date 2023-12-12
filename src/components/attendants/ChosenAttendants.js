d9d384b70afdd66a6974e0768ff7e5df7f090c62
Casper Pedersen <116882875+cp361@users.noreply.github.com>
2023-12-12 20:34:08 +0100

    ryddet lidt op

--- src/components/attendants/ChosenAttendants.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import useMyContext from "@/context/my-context";
-
-const ChosenAttendants = () => {
-    const { attendants } = useMyContext();
-    return <div>Attendants <br/> <span className='chosen-text'>{attendants}</span></div>;
-}
-
-export default ChosenAttendants
