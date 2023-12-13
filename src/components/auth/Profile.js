ce9dc3573db969f7a99ae2943693a6a7e7e75cfd
Casper Pedersen <116882875+cp361@users.noreply.github.com>
2023-12-13 14:34:17 +0100

    styles

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -29,7 +29,6 @@ export function Profile() {
         </div>
         <div className='profile-text'>
 
-          {/* Vi skal også have lavet dette om til, så den viser navnet på brugeren der er logget ind */}
           <h1>Hi {UserEmail} </h1>
           <p>Welcome back!</p>
         </div>
